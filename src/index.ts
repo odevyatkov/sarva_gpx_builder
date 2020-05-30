fe5a94cd90ac7c59d813b98e9bad4937c7d39557
jane-deviatkova <tolarien@gmail.com>
2020-05-30 15:58:32 +0300

    add copyright

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ try {
     toPoint: argv.toPoint,
   };
   const extent: [number,number,number,number] = getExtent(data);
+  console.log(extent);
   const fileName: string = `${argv.name || getDefaultFileName(data)}.gpx`;
 
   const grid = buildGrid(extent, data.cell);
